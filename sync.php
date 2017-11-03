093bf4d41f4f9b3da96c68436d46697ee12fa261
drsupport <help@dr.support>
2017-11-03 14:28:58 +0300

    fix

--- sync.php
+++ sync.php
@@ -24,6 +24,7 @@ $rows = mysqli_query($db, "
 	LEFT JOIN groups ON groups.id = product.group_id
 	LEFT JOIN categories ON categories.id = groups.category_id
 ");
+
 mysqli_query($showcase_db, "TRUNCATE TABLE `varieties`");
 foreach($rows as $row){
 	mysqli_query($showcase_db, "
@@ -74,8 +75,18 @@ $rows = mysqli_query($db, "
 		sizes.id,
 		sizes.size_id,
 		sizes.variety_id,
+		groups.showroom_id,
 		sizes.amount
 	FROM `size_variety` AS sizes
+	LEFT JOIN `varieties` as product ON sizes.variety_id = product.id
+	LEFT JOIN groups ON groups.id = (
+	    SELECT 
+	    groups.id
+	    FROM groups
+	    WHERE groups.id = product.group_id
+	    LIMIT 1
+	)
+	WHERE groups.showroom_id = 3
 ");
 mysqli_query($showcase_db, "TRUNCATE TABLE `products_sizes`");
 foreach($rows as $row){
@@ -90,14 +101,15 @@ foreach($rows as $row){
 			NULL, 
 			".$row['size_id'].", 
 			".$row['variety_id'].", 
-			5, 
+			".$row['showroom_id'].", 
 			".$row['amount']."
 		);
 	");
 }
 
-mysqli_query($showcase_db, "CREATE TABLE `ladyshowroom`.`photos` SELECT * FROM `admin.ladyshowroom`.`photos`");
-mysqli_query($db, "DROP TABLE `ladyshowroom`.`photos`; CREATE TABLE `ladyshowroom`.`photos` SELECT * FROM `admin.ladyshowroom`.`photos`; UPDATE `ladyshowroom`.`photos` SET `photable_type` = 'product_main';");
+mysqli_query($db, "DROP TABLE `ladyshowroom`.`photos`;");
+mysqli_query($db, "CREATE TABLE `ladyshowroom`.`photos` SELECT * FROM `admin.ladyshowroom`.`photos`;");
+mysqli_query($db, "UPDATE `ladyshowroom`.`photos` SET `photable_type` = 'product_main';");
 
 //mysqli_query($showcase_db, "DROP TABLE `products_sizes`");
 //mysqli_query($db, "RENAME TABLE `admin.ladyshowroom`.`products_sizes` TO `ladyshowroom`.`products_sizes`");
