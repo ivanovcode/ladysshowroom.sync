9a8f740c010557003117090d07fd6c3981ba6d80
seavalley <development@ivanov.site>
2018-11-01 17:12:24 +0300

    deploy

--- index.php
+++ index.php
@@ -18,90 +18,18 @@ function connect($db, $p) {
 function disconnect($db){
     mysqli_close($db);
 }
-function getSizes($db){
+function getProducts($db, $id_category){
     $query = "
         SELECT
-        s.type,
-        s.id,
-        s.value
-        FROM (
-              SELECT id, rus value, 'rus' type FROM sizes
-              union all
-              SELECT id, eur value, 'eur' type FROM sizes
-              union all
-              SELECT id, usa value, 'usa' type FROM sizes
-        ) AS s
-        WHERE s.value IS NOT NULL
-    ";
-    $rows = mysqli_query($db, $query);
-    if(!$rows) push('getSizes(): no records', 'error');
-    return mysqli_fetch_all($rows,MYSQLI_ASSOC);
-}
-function getColors($db){
-    $query = "
-        SELECT       
-        colors.id,
-        colors.title,
-        colors.hex
+        pc.product_id
         FROM
-        colors
-    ";
-    $rows = mysqli_query($db, $query);
-    if(!$rows) push('getColors(): no records', 'error');
-    return mysqli_fetch_all($rows,MYSQLI_ASSOC);
-}
-function getCategories($db){
-    $query = "
-        SELECT       
-        categories.id,
-        categories.title
-        FROM
-        categories
-    ";
-    $rows = mysqli_query($db, $query);
-    if(!$rows) push('getCategories(): no records', 'error');
-    return mysqli_fetch_all($rows,MYSQLI_ASSOC);
-}
-function getProducts($db){
-    $query = "        
-        SELECT
-        products.id,
-        products.title,
-        products.article,
-        products.id as sorting,
-        '' as barcode,
-        products.hide as status,
-        IF(brands.id IS NULL, NULL, CONCAT('[{\"id\":\"',brands.id,'\", \"title\":\"',brands.title,'\"}]')) as brand,
-        IF(groups.id IS NULL, NULL, CONCAT('[{\"id\":\"',groups.id,'\", \"title\":\"',groups.title,'\"}]')) as `group`,
-        CONCAT('[', GROUP_CONCAT(CONCAT('{\"id\":\"',categories.id,'\", \"title\":\"',categories.title,'\"}')), ']') as categories,
-        IF(products.type_size_id IS NULL, NULL, CONCAT('[{\"type\":\"',
-          CASE products.type_size_id
-              WHEN '1' then 'rus'
-              WHEN '2' then 'eur'
-              WHEN '3' then 'usa'
-          END
-        ,'\"}]')) as size,
-        IF(products.place IS NULL, NULL, CONCAT('[{\"title\":\"Центральный\", \"code\":\"',products.place,'\"}]')) as place,
-        IF(products.price IS NULL, NULL, CONCAT('[{\"purchase\":\"',products.price_purchase,'\", \"retail\":\"',products.price,'\", \"discount\":\"',products.price,'\"}]')) as price,
-        IF(colors.id IS NULL, NULL, CONCAT('[{\"id\":\"',colors.id,'\", \"title\":\"',colors.title,'\", \"hex\":\"',colors.hex,'\"}]')) as color,
-        '' as consist,
-        products.discription,
-        IF(products.thumbnail IS NULL, NULL, CONCAT('[{\"type\":\"primary\", \"url\":\"http://admin.ladysshowroom.ru/uploads/products/',products.thumbnail,'\"}]')) as thumbnails
-        FROM
-        products
-        LEFT JOIN products_categories as pc ON pc.product_id = products.id
-        LEFT JOIN categories ON categories.id = pc.category_id
-        LEFT JOIN groups ON groups.id = products.group_id
-        LEFT JOIN brands ON brands.id = products.brand_id
-        LEFT JOIN colors ON colors.id = products.color_id
-          WHERE
-        products.`delete` = 0
-        GROUP BY  products.id
-    ";
+        products_categories as pc
+        WHERE pc.category_id = ".$id_category;
     $rows = mysqli_query($db, $query);
     if(!$rows) push('getProducts(): no records', 'error');
     return mysqli_fetch_all($rows,MYSQLI_ASSOC);
 }
+
 
 $response = [];
 $response['collection'] = [];
@@ -109,52 +37,19 @@ $config = parse_ini_file('config.ini', true);
 $db =  connect('development', $config);
 mysqli_select_db($db, $config['development']['dbname']);
 
+$links = array("6"=>"1", "7"=>"2", "8"=>"3", "9"=>"4");
 
-$rows = getSizes($db);
-$sizes = [];
-foreach ($rows as $key => $row) {
-    if(!is_array($sizes[$row['type']])) $sizes[$row['type']] = [];
-    array_push($sizes[$row['type']], array('id' => $row['id'], 'label' => $row['value']));
+
+foreach ($links as $id_category => $id_brand) {
+    $rows = getProducts($db, $key);
+    foreach ($rows as $key => $row) {
+        print_r($row);
+        mysqli_query($db, "UPDATE `products` SET `brand_id` = '".$id_brand."' WHERE `products`.`id` = " . $row['product_id']);
+        mysqli_query($db, "DELETE FROM `products_categories` WHERE `products_categories`.`product_id` = " . $row['product_id'] . " AND `products_categories`.`category_id` = ".$id_category);
+    }
 }
 unset($rows);
 
-$rows = getProducts($db);
-$products = [];
-foreach ($rows as $key => $row) {
-
-    $row['brand'] = json_decode($row['brand'], JSON_UNESCAPED_SLASHES);
-    $row['group'] = json_decode($row['group'], JSON_UNESCAPED_SLASHES);
-    $row['categories'] = json_decode($row['categories'], JSON_UNESCAPED_SLASHES);
-    $row['size'] = json_decode($row['size'], JSON_UNESCAPED_SLASHES);
-    $row['place'] = json_decode($row['place'], JSON_UNESCAPED_SLASHES);
-    $row['price'] = json_decode($row['price'], JSON_UNESCAPED_SLASHES);
-    $row['color'] = json_decode($row['color'], JSON_UNESCAPED_SLASHES);
-    $row['thumbnails'] = json_decode($row['thumbnails'], JSON_UNESCAPED_SLASHES);
-    $products[md5($row['id'])] = $row;
-}
-unset($rows);
-
-$rows = getColors($db);
-$colors = [];
-foreach ($rows as $key => $row) {
-    if(!is_array($colors[$row['id']])) $colors[$row['id']] = [];
-    array_push($colors[$row['id']], array('id' => $row['id'], 'title' => $row['title'], 'hex' => $row['hex']));
-}
-unset($rows);
-
-$rows = getCategories($db);
-$categories = [];
-foreach ($rows as $key => $row) {
-    if(!is_array($categories[$row['id']])) $categories[$row['id']] = [];
-    array_push($categories[$row['id']], array('id' => $row['id'], 'title' => $row['title']));
-}
-unset($rows);
-
-$response['collection']['stocks']['5']['id'] = '5';
-$response['collection']['stocks']['5']['title'] = 'Центральный';
-$response['collection']['categories'] = $categories;
-$response['collection']['colors'] = $colors;
-$response['collection']['sizes'] = $sizes;
 $response['collection']['products'] = $products;
 echo json_encode($response, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
 disconnect($db);
