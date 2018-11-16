f1f2cdad7c8d1d3298ca65595ae7c8d49006b679
seavalley <development@ivanov.site>
2018-11-16 14:23:40 +0300

    add csv to change groups

--- index.php
+++ index.php
@@ -18,7 +18,19 @@ function connect($db, $p) {
 function disconnect($db){
     mysqli_close($db);
 }
+function getGroup($db, $title_group){
+    $query = "
+        SELECT
+        groups.id
+        FROM
+        groups
+        WHERE groups.title = '".$title_group."'";
 
+    $row = mysqli_query($db, $query);
+    if(!$row) push('getGroup(): no records', 'error');
+    $row = mysqli_fetch_array($row,MYSQLI_ASSOC);
+    return $row['id'];
+}
 function updateProduct($db, $row){
     $result = mysqli_query($db, "
         UPDATE `products` SET `products`.`brand_id` = '4' WHERE `title` = '$row[0]';
@@ -30,8 +42,15 @@ $db =  connect('development', $config);
 mysqli_select_db($db, $config['development']['dbname']);
 $rows = array_map('str_getcsv', file('db.csv'));
 foreach ($rows as $key => $row) {
-    print_r($row);
-    updateProduct($db, $row);
+    $id_group = getGroup($db, $row[1]);
+    if(empty($id_group)) {
+        echo $row[1]. " нет такой группы";
+        die();
+    } else {
+        echo $id_group;
+    }
+
+    //updateProduct($db, $row);
 }
 disconnect($db);
 ?>
