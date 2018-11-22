37f56ae8a35bca0912a5949c0eac5ba92b8c389f
seavalley <development@ivanov.site>
2018-11-22 17:07:58 +0300

    deploy

--- index.php
+++ index.php
@@ -11,8 +11,8 @@ function push($data, $name, $die=false, $clear=false, $msg=''){
     if ($die) die($msg);
 }
 
-function _isCurl(){
-    return function_exists('curl_version');
+function isValidJSON($data){
+   return true;
 }
 
 function connect($db, $p) {
@@ -26,70 +26,22 @@ function disconnect($db){
     mysqli_close($db);
 }
 
-function getQuantitiesFrom1C(){
-    if (!_iscurl()) push('curl is disabled', 'error', true);
-    $curl = curl_init();
-    curl_setopt_array($curl, array(
-        CURLOPT_URL => "http://cloud.itone.ru/LADYSSHOWROOM_UNF/hs/atnApi/ProductInfo",
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => "UTF-8",
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 30,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => "POST",
-        /*CURLOPT_POSTFIELDS => "[\r\n  {\r\n    \"id\": \"adr 1\",\r\n    \"original-address\": \"".$_POST['original_address']."\"\r\n  }\r\n]",*/
-        CURLOPT_POSTFIELDS => "{\"product\": \"\"}",
-        CURLOPT_HTTPHEADER => array(
-            "Authorization: Basic " . base64_encode("itone" . ":" . "itone"),
-            "cache-control: no-cache",
-            "content-type: application/json"
-        ),
-    ));
-    $data = curl_exec($curl); $error = curl_error($curl); curl_close($curl);
-    if ($error) push('request failed: '.var_dump($error), 'error', true);
-    return json_decode($data, true);
-}
-
 $config = parse_ini_file('config.ini', true);
 $db =  connect('development', $config);
 mysqli_select_db($db, $config['development']['dbname']);
 
-$rows = getQuantitiesFrom1C();
-if(empty($rows)) push('response empty', 'error', true);
-mysqli_query($db, "SET FOREIGN_KEY_CHECKS = 0;");
-mysqli_query($db, "TRUNCATE table catalog;");
-mysqli_query($db, "SET FOREIGN_KEY_CHECKS = 1;");
 
-//file_put_contents('data.json', json_encode($rows['products'], JSON_UNESCAPED_UNICODE));
-foreach ($rows['products'] as $key => $product) {
-    $product['stock'] = "5";
-    foreach ($product['onhand'] as $key => $quantity) {
-        $query = "
-                INSERT IGNORE INTO  `catalog` 
-                (
-                    `id` ,
-                    `product_id` ,
-                    `size_id` ,
-                    `showroom_id` ,
-                    `place_id` ,
-                    `quantity`
-                )
-                VALUES 
-                (NULL ,  '" . $product['id'] . "',  '" . $quantity['sizeid'] . "',  '" . $product['stock'] . "',  NULL,  '" . $quantity['qty'] . "')
-                ON DUPLICATE KEY UPDATE  quantity=" . $quantity['qty']
-        ;
-        //echo $query."\t"."\t";
-        $result = mysqli_query($db, $query);
-        $id = mysqli_insert_id($db); ($id>0?$created++:$updated++);
-    }
+if(empty($_POST)) push('no data in request', 'error', true);
+if(isValidJSON($_POST)) push('not valid json in request', 'error', true);
 
 
-}
 disconnect($db);
 
+
+$quantities = [];
+array_push($quantities, array('updated' => 'success'));
+
 $response = [];
-/*iconv(mb_detect_encoding($data, mb_detect_order(), true), "UTF-8", $data);*/
-$response['result']['created'] = ($created>0?$created:0);
-$response['result']['updated'] = ($updated>0?$updated:0);
+$response['quantities'] = $quantities;
 echo json_encode($response, JSON_UNESCAPED_UNICODE );
 ?>
