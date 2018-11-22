b09c88281d058c75d9fe2da33be897e200257111
seavalley <development@ivanov.site>
2018-11-22 22:32:25 +0300

    fix

--- index.php
+++ index.php
@@ -26,11 +26,62 @@ function disconnect($db){
     mysqli_close($db);
 }
 
-function getQuantitiesFrom1C(){
+function getOrders($db){
+    $query = "
+        SELECT
+        o.id,
+        IF(o.number IS NULL,'', o.number) as number,
+        o.created_at as created,
+        '' as updated,
+        o.amount as price,
+        o.discount,
+        o.amount-(o.amount/100)*o.discount as sum,
+        o.amount-(o.amount/100)*o.discount + d.price as total_sum,
+        'true' as owerwrite,
+        0 as is_paid,
+        o.discription as comment,
+        IF(r.id IS NULL, NULL, CONCAT('[{\"id\":\"',p.id,'\",\"size\": {\"id\": \"',r.size_id,'\", \"type_id\": \"',p.type_size_id,'\"},\"quantity\":\"',r.quantity,'\",\"price\":\"',p.price,'\",\"discount\":\"',IF(r.discount IS NULL,0,r.discount),'\", \"sum\":\"',(p.price*r.quantity)-(((p.price*r.quantity)/100)*IF(r.discount IS NULL,0,r.discount)),'\"}]')) as products,
+        IF(o.delivery_id IS NULL, NULL, CONCAT('[{\"id\":\"',d.id,'\",\"title\":\"',d.title,'\",\"address\":\"',o.address,'\",\"price\":\"',d.price,'\"}]')) as delivery,
+        IF(o.client_id IS NULL, NULL, CONCAT('[{\"id\":\"',c.id,'\",\"name\":\"',c.name,'\",\"phone\":\"',c.phone,'\",\"email\":\"\",\"overwrite\":\"true\"}]')) as client,
+        o.payments,
+        cert.id as cert_id,
+        cert.code as cert_code,
+        cert.deposit as cert_deposit,
+        cert.expiry_at as cert_expiry
+        FROM
+        orders as o
+        LEFT JOIN reserve as r ON r.order_id = o.id
+        LEFT JOIN products p on p.id = r.product_id
+        LEFT JOIN types_deliveries d on d.id = o.delivery_id
+        LEFT JOIN clients c on c.id = o.client_id
+        LEFT JOIN certificates cert on cert.order_id = o.id
+        WHERE o.number IS NULL AND o.payments IS NULL AND o.status <> 6
+        GROUP BY o.id
+    ";
+    $rows = mysqli_query($db, $query);
+    if(!$rows) push('getOrders(): no records', 'error');
+    return mysqli_fetch_all($rows,MYSQLI_ASSOC);
+}
+
+function getColors($db){
+    $query = "
+        SELECT       
+        colors.id,
+        colors.title,
+        colors.hex
+        FROM
+        colors
+    ";
+    $rows = mysqli_query($db, $query);
+    if(!$rows) push('getColors(): no records', 'error');
+    return mysqli_fetch_all($rows,MYSQLI_ASSOC);
+}
+
+function setQuantitiesIn1C($response){
     if (!_iscurl()) push('curl is disabled', 'error', true);
     $curl = curl_init();
     curl_setopt_array($curl, array(
-        CURLOPT_URL => "http://cloud.itone.ru/LADYSSHOWROOM_UNF/hs/atnApi/ProductInfo",
+        CURLOPT_URL => "http://office.itone.ru/LADYSSHOWROOM_UNF_TEST/hs/atnApi/Order",
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => "UTF-8",
         CURLOPT_MAXREDIRS => 10,
@@ -38,7 +89,8 @@ function getQuantitiesFrom1C(){
         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
         CURLOPT_CUSTOMREQUEST => "POST",
         /*CURLOPT_POSTFIELDS => "[\r\n  {\r\n    \"id\": \"adr 1\",\r\n    \"original-address\": \"".$_POST['original_address']."\"\r\n  }\r\n]",*/
-        CURLOPT_POSTFIELDS => "{\"product\": \"\"}",
+        /*CURLOPT_POSTFIELDS => "{\"product\": \"\"}",*/
+        CURLOPT_POSTFIELDS => $response,
         CURLOPT_HTTPHEADER => array(
             "Authorization: Basic " . base64_encode("itone" . ":" . "itone"),
             "cache-control: no-cache",
@@ -54,42 +106,82 @@ $config = parse_ini_file('config.ini', true);
 $db =  connect('development', $config);
 mysqli_select_db($db, $config['development']['dbname']);
 
-$rows = getQuantitiesFrom1C();
-if(empty($rows)) push('response empty', 'error', true);
-mysqli_query($db, "SET FOREIGN_KEY_CHECKS = 0;");
-mysqli_query($db, "TRUNCATE table catalog;");
-mysqli_query($db, "SET FOREIGN_KEY_CHECKS = 1;");
-
-//file_put_contents('data.json', json_encode($rows['products'], JSON_UNESCAPED_UNICODE));
-foreach ($rows['products'] as $key => $product) {
-    $product['stock'] = "5";
-    foreach ($product['onhand'] as $key => $quantity) {
-        $query = "
-                INSERT IGNORE INTO  `catalog` 
-                (
-                    `id` ,
-                    `product_id` ,
-                    `size_id` ,
-                    `showroom_id` ,
-                    `place_id` ,
-                    `quantity`
-                )
-                VALUES 
-                (NULL ,  '" . $product['id'] . "',  '" . $quantity['sizeid'] . "',  '" . $product['stock'] . "',  NULL,  '" . $quantity['qty'] . "')
-                ON DUPLICATE KEY UPDATE  quantity=" . $quantity['qty']
-        ;
-        //echo $query."\t"."\t";
-        $result = mysqli_query($db, $query);
-        $id = mysqli_insert_id($db); ($id>0?$created++:$updated++);
+$rows = getOrders($db);
+$orders = [];
+foreach ($rows as $key => $row) {
+    $row['price'] = number_format(stripos($row['price'], '.')?strstr( $row['price'], '.', true):$row['price'],2, '.', '');
+    $row['sum'] = number_format(stripos($row['sum'], '.')?strstr( $row['sum'], '.', true):$row['sum'],2, '.', '');
+    $row['total_sum'] = number_format(stripos($row['total_sum'], '.')?strstr( $row['total_sum'], '.', true):$row['total_sum'],2, '.', '');
+    $row['products'] = json_decode($row['products'], JSON_UNESCAPED_SLASHES);
+    $row['delivery'] = json_decode($row['delivery'], JSON_UNESCAPED_SLASHES);
+    $row['client'] = json_decode($row['client'], JSON_UNESCAPED_SLASHES);
+    $row['payments'] = json_decode($row['payments'], JSON_UNESCAPED_SLASHES);
+    if($row['products']) {
+        foreach ($row['products'] as $id_product => $product) {
+            $row['products'][$id_product]['price'] = number_format(stripos($product['price'], '.') ? strstr($product['price'], '.', true) : $product['price'], 2, '.', '');
+            $row['products'][$id_product]['sum'] = number_format(stripos($product['sum'], '.') ? strstr($product['sum'], '.', true) : $product['sum'], 2, '.', '');
+        }
+    } else {
+        $row['products'] = [];
     }
 
+    if($row['delivery']) {
+        foreach ($row['delivery'] as $id_delivery => $delivery) {
+            $row['delivery'][$id_delivery]['price'] = number_format(stripos($delivery['price'], '.') ? strstr($delivery['price'], '.', true) : $delivery['price'], 2, '.', '');
+        }
+    } else {
+        $row['delivery'] = [];
+    }
 
+    if($row['payments']) {
+        foreach ($row['payments'] as $id_payment => $payment) {
+            if($payment['id']==4 || $payment['id']==1) {
+                $payment['id'] = 1;
+                $payment['title'] = "Банковский перевод";
+            }
+            if($payment['id']==3) {
+                $payment['id'] = 4;
+                $payment['title'] = "Сертификат";
+
+            }
+            if($payment['id']==2) {
+                $payment['id'] = 2;
+                $payment['title'] = "Наличные";
+            }
+            $row['payments'][$id_payment]['id'] = $payment['id'];
+            $row['payments'][$id_payment]['title'] = $payment['title'];
+            $row['payments'][$id_payment]['amount'] = number_format(stripos($payment['amount'], '.') ? strstr($payment['amount'], '.', true) : $payment['amount'], 2, '.', '');
+            if(!empty($row['cert_id']) && $payment['id']==4) {
+                $certificate = [];
+                $certificate["id"] =$row['cert_id'];
+                $certificate["code"]= $row['cert_code'];
+                $certificate["deposit"]= $row['cert_deposit'];
+                $certificate["expiry"]= $row['cert_expiry'];
+                $row['payments'][$id_payment]['certificate'] = $certificate;
+            }
+            if($payment['id']==1) {
+                $row['payments'][$id_payment]['wallet_id'] = 1;
+            }
+
+        }
+    } else {
+        $row['payments'] = [];
+    }
+    unset($row['cert_id']);
+    unset($row['cert_code']);
+    unset($row['cert_deposit']);
+    unset($row['cert_expiry']);
+    $orders[$row['id']] = $row;
 }
+unset($rows);
+
+
 disconnect($db);
 
 $response = [];
 /*iconv(mb_detect_encoding($data, mb_detect_order(), true), "UTF-8", $data);*/
-$response['result']['created'] = ($created>0?$created:0);
-$response['result']['updated'] = ($updated>0?$updated:0);
-echo json_encode($response, JSON_UNESCAPED_UNICODE );
+$response['collection']['orders'] = $orders;
+//echo json_encode($response, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT );
+$response = setQuantitiesIn1C(json_encode($response, JSON_UNESCAPED_UNICODE));
+echo json_encode($response, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT );
 ?>
