1e544aafc8474d4971535769767e9fdc04f61013
seavalley <development@ivanov.site>
2018-11-22 17:31:09 +0300

    fix

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ mysqli_select_db($db, $config['development']['dbname']);
 $POST = file_get_contents('php://input');
 
 
-file_put_contents('post.json', 'POST: '.$POST);
+file_put_contents('post.json', 'POST: '.json_decode($POST));
 file_put_contents('_post.json', '_POST: '.json_encode($_POST, JSON_UNESCAPED_UNICODE));
 //if(empty($_POST)) push('no data in request', 'error', true);
 //if(!isValidJSON($_POST)) push('not valid json in request', 'error', true);
