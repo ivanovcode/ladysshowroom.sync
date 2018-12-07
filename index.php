5dc1982f119a384c17eef36d7d3f95cc64994989
seavalley <development@ivanov.site>
2018-12-07 20:25:28 +0300

    add

--- index.php
+++ index.php
@@ -3,12 +3,18 @@ header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json; charset=utf-8');
 error_reporting(0);
 
-$response = json_encode($_POST);
+echo 'POST'."\t";
+var_dump(json_encode($_POST));
+
+echo 'php://input'."\t";
+var_dump(file_get_contents('php://input'));
+
+/*$response = json_encode($_POST);
 if(empty($response)) echo 'response no POST in request';
 file_put_contents('data.json', $response);
 
 $response = file_get_contents('php://input');
 if(empty($response)) echo 'response no php://input in request';
-file_put_contents('data.json', $response);
+file_put_contents('data.json', $response);*/
 
 ?>
