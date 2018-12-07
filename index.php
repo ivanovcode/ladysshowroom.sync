fb5a9201b6fe3cbc045f253a872cd9b4e9c5e24c
seavalley <development@ivanov.site>
2018-12-07 20:32:37 +0300

    add

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json; charset=utf-8');
 error_reporting(0);
 
-
+if ($_GET['auth'] != 'd41d8cd98f00b204e9800998ecf8427e') die('access denied');
 file_put_contents('post.json', json_encode($_POST));
 file_put_contents('input.json', file_get_contents('php://input'));
 
