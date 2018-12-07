6a2cd824ee3a397ec6e5dde401db2fa9de8ce124
seavalley <development@ivanov.site>
2018-12-07 21:46:33 +0300

    fix

--- index.php
+++ index.php
@@ -3,6 +3,9 @@ header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json; charset=utf-8');
 error_reporting(0);
 
+function _isCurl(){
+    return function_exists('curl_version');
+}
 function isValidJSON($data){
     return true;
 }
@@ -13,6 +16,33 @@ function push($data, $name, $die=false, $clear=false, $msg=''){
     fclose($fp);
     if ($die) die($msg);
 }
+function getTelegram($method, $request) {
+    if (!_iscurl()) push('curl is disabled', 'error', true);
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://api.telegram.org/bot735731689:AAHEZzTKNBUJcURAxOtG6ikj6kNwc7h064c/".$method/*."?chat_id=190049461&text=%D0%9F%D1%80%D0%B8%D0%B2%D0%B5%D1%82"*/,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "UTF-8",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 500,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        /*CURLOPT_POSTFIELDS => "[\r\n  {\r\n    \"id\": \"adr 1\",\r\n    \"original-address\": \"".$_POST['original_address']."\"\r\n  }\r\n]",*/
+        /*CURLOPT_POSTFIELDS => "{\"product\": \"\"}",*/
+        CURLOPT_POSTFIELDS => $request,
+        CURLOPT_HTTPHEADER => array(
+            /*"Authorization: Basic " . base64_encode("itone" . ":" . "itone"),*/
+            "cache-control: no-cache",
+            "content-type: application/json"
+        ),
+    ));
+    $data = curl_exec($curl); $error = curl_error($curl); curl_close($curl);
+    if ($error) push('curl request failed: ' . var_dump($error), 'error', true);
+    return json_decode($data, true);
+}
+
+
+
 
 if ($_GET['auth'] != 'd41d8cd98f00b204e9800998ecf8427e') push('access denied', 'error', true);
 $POST = file_get_contents('php://input');
@@ -20,10 +50,15 @@ if(empty($POST)) push('no data in request', 'error', true);
 
 $rows = json_decode($POST, true);
 if(!isValidJSON($POST) || $rows === null) push('not valid json in request', 'error', true);
+if(empty($rows['message']['chat']['id']) && empty($rows['message']['chat']['first_name']) && empty($rows['message']['text'])) push('no require value in request', 'error', true);
 
-$chat_id = $rows['message']['chat']['id'];
-$command = $rows['message']['text'];
-push('chat: '.$chat_id.' command: '.$command, 'access');
+$request = [];
+$request['chat_id'] = $rows['message']['chat']['id'];
+$request['text'] = 'Привет, '.$rows['message']['chat']['first_name'].'!';
+$response = getTelegram('sendMessage', json_encode($request));
+
+push(var_dump($response), 'access');
+
 
 file_put_contents('input.json', json_encode($rows['message']));
 
