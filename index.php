e10b8bc6a7a058cc386fcd59cdb5e2949a6bda1d
seavalley <development@ivanov.site>
2018-12-07 23:00:15 +0300

    fix

--- index.php
+++ index.php
@@ -19,7 +19,6 @@ function push($data, $name, $die=false, $clear=false, $msg=''){
 
 function getTelegram($method, $request, $chat_id) {
     if (!_iscurl()) push('curl is disabled', 'error', true);
-    $fp = fopen('curl.log', 'w');
     $website="https://api.telegram.org/bot735731689:AAHEZzTKNBUJcURAxOtG6ikj6kNwc7h064c";
     $params=[
         'chat_id'=>$chat_id,
@@ -31,11 +30,11 @@ function getTelegram($method, $request, $chat_id) {
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, ($params));
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_VERBOSE, 1);
-    curl_setopt($ch, CURLOPT_STDERR, $fp);
+    curl_setopt($ch, CURLOPT_VERBOSE, true);
+    curl_setopt($ch, CURLOPT_STDERR, fopen('php://stderr', 'w'));
 
-    $data = curl_exec($ch); $error = curl_error($ch); curl_close($ch);
-    if ($error) push('curl request failed: ' . var_dump($error), 'error', true);
+    $data = curl_exec($ch); $info = curl_getinfo($ch); $error = curl_error($ch); curl_close($ch);
+    if ($error) push('curl request failed: ' . var_dump($info), 'error', true);
     return json_decode($data, true);
 
 }
