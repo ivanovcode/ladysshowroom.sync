d10d1e7c8cb3e0985c17addfbd737640aa46dfe9
seavalley <development@ivanov.site>
2018-12-07 21:16:45 +0300

    fix

--- index.php
+++ index.php
@@ -3,9 +3,24 @@ header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json; charset=utf-8');
 error_reporting(0);
 
-if ($_GET['auth'] != 'd41d8cd98f00b204e9800998ecf8427e') die('access denied');
-file_put_contents('post.json', json_encode($_POST));
-file_put_contents('input.json', file_get_contents('php://input'));
+function isValidJSON($data){
+    return true;
+}
+function push($data, $name, $die=false, $clear=false, $msg=''){
+    if ($clear) unlink($name.'.log');
+    $fp = fopen($name.'.log', 'a');
+    fwrite($fp, date("d.m.y").' '.date("H:i:s").' | '.$data . PHP_EOL);
+    fclose($fp);
+    if ($die) die($msg);
+}
 
+if ($_GET['auth'] != 'd41d8cd98f00b204e9800998ecf8427e') push('access denied', 'error', true);
+$POST = file_get_contents('php://input');
+if(empty($POST)) push('no data in request', 'error', true);
+
+$rows = json_decode($POST, true);
+if(!isValidJSON($POST) || $rows === null) push('not valid json in request', 'error', true);
+
+file_put_contents('input.json', json_encode($rows['message']));
 
 ?>
