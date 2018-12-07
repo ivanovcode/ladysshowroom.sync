0ebd4e869ab0b2683dce27c654ee3c6715006587
seavalley <development@ivanov.site>
2018-12-07 22:56:45 +0300

    fix

--- index.php
+++ index.php
@@ -19,6 +19,7 @@ function push($data, $name, $die=false, $clear=false, $msg=''){
 
 function getTelegram($method, $request, $chat_id) {
     if (!_iscurl()) push('curl is disabled', 'error', true);
+    $fp = fopen('curl.log', 'w');
     $website="https://api.telegram.org/bot735731689:AAHEZzTKNBUJcURAxOtG6ikj6kNwc7h064c";
     $params=[
         'chat_id'=>$chat_id,
@@ -30,6 +31,8 @@ function getTelegram($method, $request, $chat_id) {
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, ($params));
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_VERBOSE, 1);
+    curl_setopt($ch, CURLOPT_STDERR, $fp);
 
     $data = curl_exec($ch); $error = curl_error($ch); curl_close($ch);
     if ($error) push('curl request failed: ' . var_dump($error), 'error', true);
