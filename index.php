86b00ba51a8cec38b94ea0d508c03e5984617ca7
seavalley <development@ivanov.site>
2018-12-08 20:39:53 +0300

    fix

--- index.php
+++ index.php
@@ -110,7 +110,9 @@
             $request['text'] .= '<b>Выбери нужный раздел</b> 👇';
             $request['reply_markup'] = json_encode(array('keyboard' => array(
                 array(
-                    array('text'=>'📗 Расходы','callback_data'=>'finance')
+                    array('text'=>'📗 Расходы','callback_data'=>'finance'),
+                    array('text'=>'◀ Вернуться','callback_data'=>'finance')
+
                 )
             )));
             $response = getTelegram('sendMessage', $request);
