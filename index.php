b275bb1cb5b69601689145bb48abbb7f2e316b1e
seavalley <development@ivanov.site>
2018-12-08 11:16:01 +0300

    fix

--- index.php
+++ index.php
@@ -17,16 +17,48 @@ function push($data, $name, $die=false, $clear=false, $msg=''){
     if ($die) die($msg);
 }
 
-function getTelegram($chat_id) {
+function getTelegram($method, $request) {
     if (!_iscurl()) push('curl is disabled', 'error', true);
 
     $proxy = 'de360.nordvpn.com:80';
     $proxyauth = 'development@ivanov.site:ivan0vv0va';
 
-    push("http://api.telegram.org/bot735731689:AAHEZzTKNBUJcURAxOtG6ikj6kNwc7h064c/sendMessage?chat_id=".$chat_id."&parse_mode=html&text=Hi", 'access');
+    push("http://api.telegram.org/bot735731689:AAHEZzTKNBUJcURAxOtG6ikj6kNwc7h064c/sendMessage?chat_id=".$request['chat_id']."&parse_mode=html&text=Hi", 'access');
     $fp = fopen('./curl.log', 'w');
 
-    if($ch = curl_init()) {
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://api.telegram.org/bot735731689:AAHEZzTKNBUJcURAxOtG6ikj6kNwc7h064c/".$method,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "UTF-8",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 500,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        /*CURLOPT_POSTFIELDS => "[\r\n  {\r\n    \"id\": \"adr 1\",\r\n    \"original-address\": \"".$_POST['original_address']."\"\r\n  }\r\n]",*/
+        CURLOPT_POSTFIELDS => "{\"chat_id\": \"".$request['chat_id']."\",\"text\": \"hi\"}",
+        /*CURLOPT_POSTFIELDS => $request,
+        CURLOPT_HTTPHEADER => array(
+            "Authorization: Basic " . base64_encode("itone" . ":" . "itone"),
+            "cache-control: no-cache",
+            "content-type: application/json"
+        ),
+        CURLOPT_POSTFIELDS => ($request),*/
+        CURLOPT_HEADER => false,
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_VERBOSE => 1,
+        CURLOPT_STDERR => $fp
+    ));
+    $data = curl_exec($curl); $error = curl_error($curl); curl_close($curl);
+    if ($error) push('curl request failed: ' . $error, 'error');
+    return json_decode($data, true);
+
+
+
+
+
+
+    /*if($ch = curl_init()) {
         curl_setopt($ch, CURLOPT_URL, "https://api.telegram.org/bot735731689:AAHEZzTKNBUJcURAxOtG6ikj6kNwc7h064c/sendMessage?chat_id=".$chat_id."&parse_mode=html&text=Hi");
         curl_setopt($ch, CURLOPT_PROXY, $proxy);
         curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
@@ -41,7 +73,8 @@ function getTelegram($chat_id) {
     }
 
     if ($error) push('curl request failed: ' . $error, 'error');
-    return json_decode($data, true);
+    return json_decode($data, true);*/
+
 
     /*if (!_iscurl()) push('curl is disabled', 'error', true);
     $website="https://api.telegram.org/bot735731689:AAHEZzTKNBUJcURAxOtG6ikj6kNwc7h064c";
@@ -64,9 +97,7 @@ function getTelegram($chat_id) {
 
 }
 
-$response = getTelegram('190049461');
-file_put_contents('response.json', json_encode($rows['message']));
-die();
+
 
 if ($_GET['auth'] != 'd41d8cd98f00b204e9800998ecf8427e') push('access denied', 'error', true);
 $POST = file_get_contents('php://input');
@@ -80,6 +111,10 @@ $request = [];
 $request['chat_id'] = $rows['message']['chat']['id'];
 $request['text'] = 'Привет, '.$rows['message']['chat']['first_name'].'!';
 
+
+$response = getTelegram('sendMessage', $request);
+file_put_contents('response.json', json_encode($rows['message']));
+die();
 
 
 $response = getTelegram($rows['message']['chat']['id']);
