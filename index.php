a67b8f55ab091c07127fe970f63aa8016e48111f
seavalley <development@ivanov.site>
2018-12-08 20:17:52 +0300

    fix

--- index.php
+++ index.php
@@ -110,9 +110,7 @@
             $request['text'] .= '<b>Выбери нужный раздел</b> 👇';
             $request['reply_markup'] = json_encode(array('keyboard' => array(
                 array(
-                    array('text'=>'📔 Расходы','callback_data'=>'finance')
-                ),
-                array(
+                    array('text'=>'📔 Расходы','callback_data'=>'finance'),
                     array('text'=>'◀ Вернуться','callback_data'=>'finance')
                 )
             )));
