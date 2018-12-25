c6119d760eeb05c7b7a56f50aa483de0447f5842
seavalley <development@ivanov.site>
2018-12-25 18:20:09 +0300

    release 20181225_1820

--- index.php
+++ index.php
@@ -31,19 +31,21 @@ function disconnect($db){
 
 function getOrders($db){
     $query = "
-        SELECT        
+        SELECT    
+        d.price as delivery_price,    
         o.id,
         IF(o.number IS NULL,'', o.number) as number,
         o.created_at as created,
         '' as updated,
         o.amount as price,
         o.discount,
+        o.discounts,
         o.amount-(o.amount/100)*o.discount as sum,
         o.amount-(o.amount/100)*o.discount + d.price as total_sum,
         'true' as overwrite,
         0 as is_paid,
         o.discription as comment,
-        CONCAT('[', GROUP_CONCAT(IF(r.id IS NULL, NULL, CONCAT('{\"id\":\"',p.id,'\",\"size\": {\"id\": \"',r.size_id,'\", \"type_id\": \"',p.type_size_id,'\"},\"quantity\":\"',r.quantity,'\",\"price\":\"',p.price,'\",\"discount\":\"',IF(r.discount IS NULL,0,r.discount),'\", \"sum\":\"',(p.price*r.quantity)-(((p.price*r.quantity)/100)*IF(r.discount IS NULL,0,r.discount)),'\"}'))),']') as products,
+        CONCAT('[', GROUP_CONCAT(IF(r.id IS NULL, NULL, CONCAT('{\"id\":\"',p.id,'\",\"reserve_id\":\"',r.id,'\",\"size\": {\"id\": \"',r.size_id,'\", \"type_id\": \"',p.type_size_id,'\"},\"quantity\":\"',r.quantity,'\",\"price\":\"',p.price,'\",\"discount\":\"',IF(r.discount IS NULL,0,r.discount),'\", \"sum\":\"',(p.price*r.quantity)-(((p.price*r.quantity)/100)*IF(r.discount IS NULL,0,r.discount)),'\"}'))),']') as products,
         IF(o.delivery_id IS NULL, NULL, CONCAT('[{\"id\":\"',d.id,'\",\"title\":\"',d.title,'\",\"address\":\"',o.address,'\",\"price\":\"',d.price,'\"}]')) as delivery,
         IF(o.client_id IS NULL, NULL, CONCAT('[{\"id\":\"',c.id,'\",\"name\":\"',c.name,'\",\"phone\":\"',c.phone,'\",\"email\":\"\",\"overwrite\":\"true\"}]')) as client,
         IF(o.user_id IS NULL, NULL, CONCAT('[{\"id\":\"',o.user_id,'\"}]')) as staff,
@@ -113,10 +115,12 @@ mysqli_select_db($db, $config['development']['dbname']);
 
 $rows = getOrders($db);
 $orders = [];
-
+$total_sum = 0;
 foreach ($rows as $key => $row) {
     $row['status'] = "1";
     //array_push($row['status'], array('id'=>'1'));
+
+
     $row['price'] = number_format(stripos($row['price'], '.')?strstr( $row['price'], '.', true):$row['price'],2, '.', '');
     $row['sum'] = number_format(stripos($row['sum'], '.')?strstr( $row['sum'], '.', true):$row['sum'],2, '.', '');
     $row['total_sum'] = number_format(stripos($row['total_sum'], '.')?strstr( $row['total_sum'], '.', true):$row['total_sum'],2, '.', '');
@@ -125,10 +129,29 @@ foreach ($rows as $key => $row) {
     $row['client'] = json_decode($row['client'], JSON_UNESCAPED_SLASHES);
     $row['staff'] = json_decode($row['staff'], JSON_UNESCAPED_SLASHES);
     $row['payments'] = json_decode($row['payments'], JSON_UNESCAPED_SLASHES);
+
+    $_discounts=[];
+    $discounts = $row['discounts'];
+    if(!empty($discounts)) {
+        $discounts = explode(",", $discounts);
+        foreach ($discounts as $discount_key => $discount) {
+            $discount = explode(":", $discount);
+            $_discounts[ substr($discount[0], 0, -1)] = $discount[1];
+        }
+    }
+    $discounts = $_discounts;
+    unset($_discounts);
+
+
+
     if($row['products']) {
         foreach ($row['products'] as $id_product => $product) {
+            $row['products'][$id_product]['discount'] = ($discounts[$product['reserve_id']]?$discounts[$product['reserve_id']]:0);
             $row['products'][$id_product]['price'] = number_format(stripos($product['price'], '.') ? strstr($product['price'], '.', true) : $product['price'], 2, '.', '');
-            $row['products'][$id_product]['sum'] = number_format(stripos($product['sum'], '.') ? strstr($product['sum'], '.', true) : $product['sum'], 2, '.', '');
+            $sum = (intval($product['price'])*intval($product['quantity']))-(((intval($product['price'])*intval($product['quantity']))/100)*intval($discounts[$product['reserve_id']]));
+            $row['products'][$id_product]['sum'] = number_format(stripos($sum, '.') ? strstr($sum, '.', true) : $sum, 2, '.', '');
+            unset($row['products'][$id_product]['reserve_id']);
+            $total_sum = $total_sum + intval($sum);
         }
     } else {
         $row['products'] = [];
@@ -141,6 +164,7 @@ foreach ($rows as $key => $row) {
             $row['datefrom'] =  $now;
             $row['dateto'] =  $now;
             $row['delivery'][$id_delivery]['price'] = number_format(stripos($delivery['price'], '.') ? strstr($delivery['price'], '.', true) : $delivery['price'], 2, '.', '');
+
         }
     } else {
         $row['delivery'] = [];
@@ -180,7 +204,18 @@ foreach ($rows as $key => $row) {
     } else {
         $row['payments'] = [];
     }
+
+   // $row['price'] = number_format(stripos($total_sum, '.')?strstr($total_sum, '.', true):$total_sum,2, '.', '');
+    $row['sum'] = $total_sum;
+    if(intval($row['discount'])>0) {
+        $row['sum'] = number_format(stripos(((intval($total_sum) / 100) * intval($row['discount'])), '.') ? strstr(((intval($total_sum) / 100) * intval($row['discount'])), '.', true) : ((intval($total_sum) / 100) * intval($row['discount'])), 2, '.', '');
+    }
+    $row['total_sum'] = number_format(stripos((intval($row['sum'])+intval($row['delivery_price'])), '.')?strstr((intval($row['sum'])+intval($row['delivery_price'])), '.', true):(intval($row['sum'])+intval($row['delivery_price'])),2, '.', '');
+    $row['sum'] = number_format(stripos($row['sum'], '.')?strstr( $row['sum'], '.', true):$row['sum'],2, '.', '');
+
+    unset($row['delivery_price']);
     unset($row['wallet']);
+    unset($row['discounts']);
     unset($row['cert_id']);
     unset($row['cert_code']);
     unset($row['cert_deposit']);
@@ -197,6 +232,8 @@ if($orders) {
     $ids =  implode (", ", array_values(array_column($orders, 'id')));
     $response = [];
     $response['collection']['orders'] = $orders;
+    /*print_r($response);
+    die();*/
     file_put_contents($now.'_request.json', json_encode($response, JSON_UNESCAPED_UNICODE));
     $response = setQuantitiesIn1C(json_encode($response, JSON_UNESCAPED_UNICODE));
     $response = json_encode($response, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
