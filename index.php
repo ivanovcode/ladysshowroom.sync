713d57478789d74c7cefcf9dda6e43ac187b5aa6
Vladimir V. Ivanov <profidela.com@gmail.com>
2019-09-24 00:02:22 +0300

    Исправлено баланс

--- index.php
+++ index.php
@@ -364,7 +364,7 @@
             LEFT JOIN `1c_cash` ca ON ca.id_staff = cw.id_1c_staff
             LEFT JOIN `1c_staffs` st ON st.id = cw.id_1c_staff
             SET w.title = CASE WHEN t.id IS NULL THEN CONCAT('Наличные ', st.firstname) ELSE t.`name` END, w.balance = CASE WHEN t.id IS NULL THEN ca.remained ELSE t.amount END
-            WHERE t.type = 'CashRegisters' AND (tw.`update` = 1 OR cw.`update` = 1) AND (ca.remained IS NOT NULL OR t.amount IS NOT NULL)
+            WHERE (t.type = 'CashRegisters' OR t.id IS NULL) AND (tw.`update` = 1 OR cw.`update` = 1) AND (ca.remained IS NOT NULL OR t.amount IS NOT NULL)
         ";
         mysqli_query($db, $q);
     }
